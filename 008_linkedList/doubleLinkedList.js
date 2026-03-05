a77be6e336a732066bfc2f28469f39f2fb81c61c
Tejas Havale <tejashavale007@gmail.com>
2026-03-05 16:31:14 +0530

    Double Linked list: Remove head, remove tail

--- 008_linkedList/doubleLinkedList.js
+++ 008_linkedList/doubleLinkedList.js
@@ -42,13 +42,51 @@ class DoubleLinkedList {
 
     displayLinkedList() {
         let temp = this.head;
-        console.log(this.head);
+        // console.log(this.head);
         let output = "";
         while (temp) {
             output += `${temp.data} `;
             temp = temp.next;
         }
         console.log("Double linked list: ", output);
+    }
+
+    removeHead() {
+        // If list is empty
+        if (!this.head) {
+            console.log("List is empty.");
+            return;
+        }
+
+        // If list contains only one node
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+            return;
+        }
+
+        let newHead = this.head.next;
+        newHead.prev = null;
+        this.head = newHead;
+    }
+
+    removeTail() {
+        // If list is empty
+        if (!this.head) {
+            console.log("List is empty.");
+            return;
+        }
+
+        // If list contains only one node
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+            return;
+        }
+
+        let newTail = this.tail.prev;
+        newTail.next = null;
+        this.tail = newTail;
     }
 }
 
@@ -58,4 +96,11 @@ dll.addNodeAtStart(8);
 dll.addNodeAtEnd(12);
 dll.addNodeAtEnd(14);
 dll.addNodeAtEnd(16);
+dll.displayLinkedList();
+
+dll.removeHead();
+dll.displayLinkedList();
+dll.removeHead();
+dll.displayLinkedList();
+dll.removeTail();
 dll.displayLinkedList();
