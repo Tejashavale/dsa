5f8391b5924ea65d0e281ed884d642f93fb21e13
Tejas Havale <tejashavale007@gmail.com>
2026-03-12 22:43:35 +0530

    Strings easy problems: Remove outer paranthesis

--- 007_strings/001_easy/001_removeOuterParanthesis.js
+++ 007_strings/001_easy/001_removeOuterParanthesis.js
@@ -25,3 +25,23 @@ const removeOuterParanthesis = (s) => {
 console.log(`removeOuterParanthesis of ${s1}: `, removeOuterParanthesis(s1));
 console.log(`removeOuterParanthesis of ${s2}: `, removeOuterParanthesis(s2));
 console.log(`removeOuterParanthesis of ${s3}: `, removeOuterParanthesis(s3));
+
+const removeOuterParanthesis2 = (s) => {
+    let output = "";
+    let counter = 0;
+
+    for (let char of s) {
+        if (char === "(") {
+            if (counter > 0) output += char;
+            counter++;
+        } else if (char === ")") {
+            counter--;
+            if (counter > 0) output += char;
+        }
+    }
+    return output;
+}
+
+console.log(`removeOuterParanthesis2 of ${s1}: `, removeOuterParanthesis2(s1));
+console.log(`removeOuterParanthesis2 of ${s2}: `, removeOuterParanthesis2(s2));
+console.log(`removeOuterParanthesis2 of ${s3}: `, removeOuterParanthesis2(s3));
